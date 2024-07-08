290e0e7cc3dfb35bd2fb0d53191bd781e98e762a
Davi Monteiro <davi321522@gmail.com>
2024-07-08 19:32:47 -0300

    att02

---
+++ Connection_To_DataBaseCSharpe/DataBase/Connection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connection_To_DataBaseCSharpe.DataBase
+{
+    internal class Connection
+    {
+        private string connectionString = "Data Source=DESKTOP-KNM3SP8;Initial Catalog=ConnectionToDataBaseCSharp;User ID=sa;Password=********;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        //a função para usar o o link string para conectar o projeto com o banco de dados
+        public SqlConnection ObterConexao()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
--- Connection_To_DataBaseCSharpe/Program.cs
+++ Connection_To_DataBaseCSharpe/Program.cs
@@ -1,8 +1,18 @@
 ﻿
+using Connection_To_DataBaseCSharpe.DataBase;
 using Connection_To_DataBaseCSharpe.Models;
 
+try
+{
+    using var connection = new Connection().ObterConexao();
+    connection.Open();
+    Console.WriteLine(connection.State);
+}
+catch(Exception ex)
+{
 
-
+    Console.WriteLine(ex.Message);
+}
 Usuario user = new Usuario("Roberto Carlos", 78, "Rua Florestino Gaviões, 78 , Florianopolis-PE");
 
 user.Menu();
