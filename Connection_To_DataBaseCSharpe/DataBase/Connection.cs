74b86a5bfc52a2c1b4b27c046312352cbaa49353
Davi Monteiro <davi321522@gmail.com>
2024-07-09 13:33:17 -0300

    att 03

--- Connection_To_DataBaseCSharpe/DataBase/Connection.cs
+++ Connection_To_DataBaseCSharpe/DataBase/Connection.cs
@@ -8,8 +8,10 @@ using System.Threading.Tasks;
 namespace Connection_To_DataBaseCSharpe.DataBase
 {
     internal class Connection
-    {
-        private string connectionString = "Data Source=DESKTOP-KNM3SP8;Initial Catalog=ConnectionToDataBaseCSharp;User ID=sa;Password=********;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+ // voce também pode consultar em um site confiável um link string de connection confiável que ja funciona, apenas altere as opções importantes como id senha e nome.
+         // Confira essas informações do string connection, como nome id e senha:
+    {                                      //nome do servidor      Nome do Banco de Dados              Id do servidor e senha
+        private string connectionString = "Server=DESKTOP-KM1NEG8;Database=TestConnectionToDataBase;User Id=sa;Password=123456;\r\n";
 
         //a função para usar o o link string para conectar o projeto com o banco de dados
         public SqlConnection ObterConexao()
--- Connection_To_DataBaseCSharpe/Program.cs
+++ Connection_To_DataBaseCSharpe/Program.cs
@@ -4,15 +4,21 @@ using Connection_To_DataBaseCSharpe.Models;
 
 try
 {
+    //inserimos a instrução using pois podemos gerenciar os recursos de conexão
     using var connection = new Connection().ObterConexao();
     connection.Open();
-    Console.WriteLine(connection.State);
+    Console.WriteLine(connection.State);//uma verificação do status da nossa conexão, ele retorna o estado da nossa conexão
 }
+//se der algo errado, e não realizar a nossa conexão, exibiremos uma mensagem:
 catch(Exception ex)
 {
 
     Console.WriteLine(ex.Message);
 }
+//como não queremos ficar executando o menu toda hora enquanto testamos nossa conexão, iremos colocar um return depois do catch
+// para que o programa execute até esta parte e pare a execução quando chegar no return;
+return;
+
 Usuario user = new Usuario("Roberto Carlos", 78, "Rua Florestino Gaviões, 78 , Florianopolis-PE");
 
 user.Menu();
